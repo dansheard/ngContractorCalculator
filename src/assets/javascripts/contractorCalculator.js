ae368fa4605d8084d91062096246334411967978
Dan Sheard <dan.c.sheard@gmail.com>
2014-11-20 22:11:49 +0000

    Finish ngContractorCalculator first pass

--- src/assets/javascripts/contractorCalculator.js
+++ src/assets/javascripts/contractorCalculator.js
@@ -1,33 +1,49 @@
 angular.module('contractorCalculator', ['ngMessages'])
   .controller('FormCtrl', ['$scope', function($scope) {
-  	$scope.submitted = false;
-
+  	
     $scope.initValues = function() {
+      $scope.submitted = false;
       $scope.dailyRate = 0;
       $scope.vat = 0;
       $scope.dailyExpenses = 0;
+      $scope.payTotal = 0;
+      $scope.dayCount = 0;
+      $scope.averageDailyPay = 0;
+      $scope.subtotal = 0;
     };
 
     $scope.initValues();
 
     $scope.clearForm = function() {
-      console.log("clearing...")
       $scope.dailyRate = null;
       $scope.vat = null;
       $scope.expenses = null;
     }; 
 
+    $scope.reset = function() {
+      $scope.contractorForm.$setPristine();
+      $scope.initValues();
+    }
+
     $scope.submit = function() {
       if ($scope.contractorForm.$valid) {
         $scope.getSubtotal();
-      } else {
-        console.log('fuck off');
-      }
+        $scope.getPayTotal();
+        $scope.dayCount += 1;
+        $scope.averageDailyPay = $scope.payTotal / $scope.dayCount;
+      } 
     };
 
     $scope.getSubtotal = function() {
       var vatPercentage = $scope.vat / 100 * $scope.dailyRate;
       $scope.subtotal = vatPercentage + $scope.dailyRate;
       return $scope.subtotal;
-    }
+    };
+
+    $scope.getPayTotal = function() {
+      var dayTotals = $scope.dailyExpenses + $scope.subtotal;
+      $scope.payTotal += dayTotals;
+      return $scope.payTotal;
+    };
+
   }]);
