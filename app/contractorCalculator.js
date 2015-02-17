06b0f6765e0012fd70445a27e10182dfac23aac3
Dan Sheard <dan.c.sheard@gmail.com>
2015-02-17 15:37:09 +0000

    add animations to the contractor calculator

--- app/contractorCalculator.js
+++ app/contractorCalculator.js
@@ -1,4 +1,4 @@
-angular.module('contractorCalculator', ['ngMessages', 'ngRoute'])
+angular.module('contractorCalculator', ['ngMessages', 'ngRoute', 'ngAnimate'])
   .config(function($routeProvider) {
     $routeProvider.when('/', {
       templateUrl: './app/home.html',
@@ -15,6 +15,20 @@ angular.module('contractorCalculator', ['ngMessages', 'ngRoute'])
     .otherwise({
         redirectTo : '/'
     })
+  })
+
+  .run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+      $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+      $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 500);
+    });
   })
 
   .controller('HomeCtrl', function() {
